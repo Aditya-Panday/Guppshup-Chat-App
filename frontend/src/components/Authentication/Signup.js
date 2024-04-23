48c78c30d63b077d2d1fc25342246bb552add6c1
Aditya-Panday <153483981+Aditya-Panday@users.noreply.github.com>
2024-04-23 11:36:45 +0530

    Update Signup.js

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -57,7 +57,7 @@ const Signup = () => {
                 },
             };
             const { data } = await axios.post(
-                "http://127.0.0.1:5000/api/user",
+                "/api/user",
                 {
                     name,
                     email,
